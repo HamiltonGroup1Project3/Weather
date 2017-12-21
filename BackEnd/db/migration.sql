b5bcfc37b07ff6cbcdbf6c4f53dbcfb85ef68188
mark <mjalbom@gmail.com>
2017-12-21 14:04:41 -0500

    db populated w/ dummy files from seed and migration

--- BackEnd/db/migration.sql
+++ BackEnd/db/migration.sql
@@ -1,20 +1,27 @@
-\c top_shelf
+\c top_shelf;
 
+--drop tables if it already exists--
+DROP TABLE if EXISTS type;
+DROP TABLE if EXISTS beer;
+
+--create type table--
+CREATE TABLE type (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
 -- create beer table --
-CREATE TABLE IF not exists beer (
+CREATE TABLE beer (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  brand VARCHAR(255) NOT NULL,
-  type_id INTEGER NOT NULL REFERENCES type,
+  brewery VARCHAR(255) NOT NULL,
+  type_id SERIAL NOT NULL REFERENCES type,
   description TEXT
 );
 
 CREATE INDEX ON beer (type_id);
 
---create type table--
-CREATE TABLE IF not exists type (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255)
-);
+
+
+
 
 
