0128ae1fde686fbb77085820582e82496a4300e5
Mohammad Badruzzaman <mohammad.badruzz@gmail.com>
2017-12-23 14:09:07 -0500

    changes

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Filter from './components/Filter';
+import BeerList from './components/BeerList';
+import NotFound from './components/NotFound';
+import Nav from './components/Nav';
+import './css/App.css'
+
+
+
+
+class App extends Component {
+    constructor(props){
+    super(props);
+    this.state = {
+      // singleBeerData: null,
+      dataLoaded: false,
+      beerList: null,
+      // addingBeerForm: null,
+      }
+    // this.
+  }
+
+componentDidMount(){
+  this.getBeers();
+}
+
+getBeers() {
+   fetch('/test')
+  .then(res => res.json())
+  .then(res =>{
+  this.state({
+  dataLoaded: true,
+  beerData: res.data.beers,
+  })
+  }
+)}
+
+  //setting State to when we add A beer
+  // AddBeer(){
+  //   this.setState({
+  //     addingBeer: true,
+  //   })
+  // }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+
+       <Header />
+       <Filter />
+       <Nav />
+       <Home />
+       <BeerList
+          beerList={this.state.beerList}
+          // dataLaoded={this.state.dataLoaded}
+          // singleBeerData={this.singleBeerData}
+      />
+
+
+
+        <h1 className="App-title">This is the Home-page</h1>
+
+        { /*
+         <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/BeerList" component={BeerList} />
+            <Route
+              path="/Beer"
+              render={props => <SingleBeer {...props} beer={this.state.beer} />}
+            />
+            <Route path="/" component={NotFound} />
+
+          </Switch>  */}
+          <Footer />
+
+      </div>
+    );
+  }
+}
+
+export default App;
