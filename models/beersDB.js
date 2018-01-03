617fae7d803f5a5dd494441a714cdab846687c99
mark <mjalbom@gmail.com>
2018-01-02 20:09:18 -0500

    changed delete function in model

--- models/beersDB.js
+++ models/beersDB.js
@@ -59,14 +59,17 @@ module.exports = {
   /* function 'deleteOneBeer' to remove a beer (from both the xref table and the beer table) */
 
   deleteOneBeer(id) {
-    return db.none(`
-      DELETE
-      FROM x_ref_table
-      WHERE beer_id = $1;
-      DELETE
-      FROM beer
-      WHERE id = $1
-      `, +id);
+    return db.none('beerDeletion', async (t) => {
+      const { id: beerID } = await t.one(`
+        DELETE FROM x_ref_table
+        WHERE beer_id = $1;
+        `, beer);
+
+      await t.one(`
+        DELETE FROM beer
+        WHERE id = $1
+        `, +id);
+      });
+    }
   }
-}
 
