1a645050ed79ddafe571b57d55895d18b3d8a070
Luke McDonald <t.luke.mcdonald@gmail.com>
2018-01-03 09:45:59 -0500

    css

--- src/components/SingleBeer.jsx
+++ src/components/SingleBeer.jsx
@@ -16,9 +16,10 @@ const SingleBeer = (props) => {
     >
 
       <div className="singleBeer">
-        {props.id}
+        {/*props.id*/}
         {/* <<  to hide later, using for reference  */ }
         <p>  {props.name} </p>
+        <br />
         <p> {props.brewery} </p>
 
 
